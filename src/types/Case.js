8dd974adae13e280a8643a0a5a51539d4aec216f
cortex05 <havinginitiative@gmail.com>
2026-05-22 15:13:47 -0500

    feat: create case saves to local storage | made styles in create case modular

--- src/types/Case.js
+++ src/types/Case.js
@@ -2,8 +2,13 @@ class Case {
   students = []
   questions = []
 
-  constructor(name) {
+  constructor(_id, name, author, location, studentNumber, questions) {
+    this._id = _id;
     this.name = name;
+    this.author = author;
+    this.location = location
+    this.studentNumber = studentNumber
+    this.questions = questions
     this.dateCreated = new Date();
   }
 
