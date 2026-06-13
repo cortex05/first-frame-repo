312b77dc910f6a4e378f6b1c7d16d87247b6fc0d
cortex05 <havinginitiative@gmail.com>
2026-06-13 17:15:16 -0500

    feat: Adjusted styling of home screen access existing case modal | format doc

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Modal from '../../components/modal/Modal';
-import styles from './HomeScreen.module.css';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "../../components/modal/Modal";
+import styles from "./HomeScreen.module.css";
 
-import useCaseStore from '../../store/useCaseStore';
+import useCaseStore from "../../store/useCaseStore";
 
 const Home = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -13,13 +13,13 @@ const Home = () => {
   const navigate = useNavigate();
 
   const handleSelectCase = (caseId) => {
-	setActiveCase(caseId);
-	setModalOpen(false);
-	navigate(`/case/${caseId}`);
-  }
+    setActiveCase(caseId);
+    setModalOpen(false);
+    navigate(`/case/${caseId}`);
+  };
 
   useEffect(() => {
-    const cases = localStorage.getItem('cases');
+    const cases = localStorage.getItem("cases");
     if (cases) {
       setStoredCases(JSON.parse(cases));
     }
@@ -28,14 +28,14 @@ const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <h1>Welcome to the Home Page!</h1>
-      <p>This is the home screen of our application. Here you will be able to access our various features.</p>
+      <p>
+        This is the home screen of our application. Here you will be able to
+        access our various features.
+      </p>
       <h3>What would you like to do?</h3>
 
       <div className={styles.actionContainer}>
-        <Link
-          to="/create-case"
-            className={styles.linkButton}
-        >
+        <Link to="/create-case" className={styles.linkButton}>
           Create New Case
         </Link>
 
@@ -52,23 +52,23 @@ const Home = () => {
         onClose={() => setModalOpen(false)}
         title="Access Existing Case"
       >
-        <div>
-        <h3>Existing Cases:</h3>
+        <div className={styles.modalContent}>
           {storedCases.length === 0 ? (
             <p>No existing cases found.</p>
           ) : (
-            <ul>
+            <div>
               {storedCases.map((c, i) => (
-                <li key={i} onClick={() => handleSelectCase(c._id)}>
-                    <button className={styles.caseLink}>
-					  {c.name} by {c.author}
-					</button>
-                </li>
+                <div 
+                  key={i} 
+                  onClick={() => handleSelectCase(c._id)}
+                  className={styles.caseItem}
+                >
+                  <span>{c.name}</span> by <span>{c.author}</span>
+                </div>
               ))}
-            </ul>
+            </div>
           )}
         </div>
-
       </Modal>
     </div>
   );
