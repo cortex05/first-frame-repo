b7c03288cc4bfe04d505264b4de8c3d0cbedd384
cortex05 <havinginitiative@gmail.com>
2026-08-20 16:24:47 -0500

    feat: added fix for console warning

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Patch HTMLCanvasElement.getContext so Konva's hit-detection canvas
+// gets willReadFrequently=true, suppressing the repeated getImageData warning.
+const _getContext = HTMLCanvasElement.prototype.getContext;
+HTMLCanvasElement.prototype.getContext = function (type, attrs) {
+  if (type === '2d') {
+    attrs = { willReadFrequently: true, ...attrs };
+  }
+  return _getContext.call(this, type, attrs);
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
